9d5ac2439793e384c71f165d2ddcf5e255fc6edb
Grimshady1990 <frankiedthomas@gmail.com>
2025-07-02 22:20:37 +0100

    finish all tasks

--- Objects/Task2/index.js
+++ Objects/Task2/index.js
@@ -18,22 +18,89 @@
 
 // Part 2
 
-let head = {
-    glasses: 1
+// let head = {
+//     glasses: 1,
+    
+    
+// };
+
+// let table = {
+//     pen: 3,
+//     __proto__:head
+    
+   
+// };
+
+// let bed = {
+//     sheet: 1,
+//     pillow: 2,
+//     __proto__:table
+    
+    
+// };
+
+// let pockets = {
+//     money: 2000,
+//     __proto__: bed
+    
+// };
+
+// console.time('head.glasses'); // Start a timer with a label
+// // Code to be benchmarked
+
+// for (let i = 0; i < 1; i++) {
+//   console.log(head.glasses);
+  
+// }
+// console.timeEnd('head.glasses');
+
+// console.time('pockets.glasses'); // Start a timer with a label
+// // Code to be benchmarked
+
+// for (let i = 0; i < 1; i++) {
+//   console.log(pockets.glasses);
+  
+// }
+// console.timeEnd('pockets.glasses');
+
+// Part 3
+
+// let animal = {
+//   eat() {
+//     this.full = true;
+//   }
+// };
+
+// let rabbit = {
+//   __proto__: animal
+// };
+
+// rabbit.eat();
+
+// Part 4
+
+let hamster = {
+  stomach: [],
+
+  eat(food) {
+    this.stomach.push(food);
+  }
 };
 
-let table = {
-    pen: 3
+let speedy = {
+  __proto__: hamster
 };
 
-let bed = {
-    sheet: 1,
-    pillow: 2
+let lazy = {
+  
 };
 
-let pockets = {
-    money: 2000
-};
+// This one found the food
+speedy.eat("apple");
+alert( speedy.stomach ); // apple
+
+// This one also has it, why? fix please.
+alert( lazy.stomach ); // apple
 
 // https://javascript.info/prototype-inheritance
 
