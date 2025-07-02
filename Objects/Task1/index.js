9f4a79c7a1adc26b14b1ff1137ff2f215f710e7e
Grimshady1990 <frankiedthomas@gmail.com>
2025-07-02 04:59:06 +0100

    Start task 2 of prototypal article
    link can be found at the foot of index.js

--- Objects/Task1/index.js
+++ Objects/Task1/index.js
@@ -30,10 +30,20 @@ this.marker = marker;
 }
 
 const player1 = new Player("john", "X");
-const player2 = new Player("dave", "O")
+const player2 = new Player("dave", "O");
 
+Player.prototype.sayHello = function() {
+    return "Hello, I'm a player";
+}
+
+console.log(player1.sayHello());
+console.log(player2.sayHello());
+console.log(player1.valueOf());
+console.log(player1.hasOwnProperty('valueOf'));
+console.log(Object.prototype.hasOwnProperty('valueOf'));
 console.log(Object.getPrototypeOf(player1) === Player.prototype); // returns true
 console.log(Object.getPrototypeOf(player2) === Player.prototype); // returns true
+console.log(Object.getPrototypeOf(Player.prototype) === Object.prototype); // true
 
 
 
---
+++ Objects/Task2/index.js
@@ -0,0 +1,42 @@
+// let animal = {
+//     jumps: null
+// };
+// let rabbit = {
+//     __proto__: animal,
+//     jumps: true
+// };
+
+// alert( rabbit.jumps );
+
+// delete rabbit.jumps;
+
+// alert( rabbit.jumps );
+
+// delete animal.jumps;
+
+// alert( rabbit.jumps );
+
+// Part 2
+
+let head = {
+    glasses: 1
+};
+
+let table = {
+    pen: 3
+};
+
+let bed = {
+    sheet: 1,
+    pillow: 2
+};
+
+let pockets = {
+    money: 2000
+};
+
+// https://javascript.info/prototype-inheritance
+
+
+
+
