167d4e7b8bf765d9cf786c8a906a0f5b60791dea
Grimshady1990 <frankiedthomas@gmail.com>
2025-07-20 18:27:32 +0100

    Complete mdn objects lesson

---
+++ Objects/Task4/index.js
@@ -0,0 +1,53 @@
+// const person = {
+//     name: {
+//         first: "Bob",
+//         last: "Smith",
+//     },
+//     age: 32,
+//     bio: function () {
+//         console.log(`${this.name.first} ${this.name.last} is ${this.age} years old`);
+//     },
+//     introduceSelf: function () {
+//         console.log(`Hi! I'm ${this.name.first}`);
+//     },
+// };
+
+// const myDataName = "height";
+// const myDataValue = "1.75m";
+// person[myDataName] = myDataValue;
+
+// person.age = 45;
+// person["name"]["last"] = "Cratchit"
+
+// console.log(person.name);
+
+// console.log(person.name.first);
+
+// console.log(person.age);
+
+// person.bio();
+
+// person.introduceSelf();
+
+// person["eyes"] = "hazel";
+// console.log(person["eyes"]);
+
+// person.farewell = function () {
+//     console.log("Bye everyone!");
+// };
+
+// person.farewell();
+// console.log(person.height);
+
+// function Person(name) {
+//     this.name = name;
+//     this.introduceSelf = function () {
+//         console.log(`Hi! I'm ${this.name}.`)
+//     };
+// }
+
+// const salva = new Person("Salva");
+// salva.introduceSelf();
+
+// const frankie = new Person("Frankie");
+// frankie.introduceSelf();
