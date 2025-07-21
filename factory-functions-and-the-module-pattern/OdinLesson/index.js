6237ebe59185e33221001beed0f2d8412106266c
Grimshady1990 <frankiedthomas@gmail.com>
2025-07-21 21:54:11 +0100

    Continue mdn closers

--- factory-functions-and-the-module-pattern/OdinLesson/index.js
+++ factory-functions-and-the-module-pattern/OdinLesson/index.js
@@ -85,18 +85,59 @@
 
 // ********************************************
 
-const e = 10;
-function sum(a) {
-    return function (b) {
-        return function (c) {
-            return function (d) {
+// const e = 10;
+// function sum(a) {
+//     return function (b) {
+//         return function (c) {
+//             return function (d) {
 
-                return a + b + c + d + e;
-            };
-        };
-    };
-}
+//                 return a + b + c + d + e;
+//             };
+//         };
+//     };
+// }
 
-console.log(sum(1)(2)(3)(4));
+// console.log(sum(1)(2)(3)(4));
+
+
+// const e = 10;
+// function sum(a) {
+//     return function sum2(b) {
+//         return function sum3(c) {
+//             return function sum4(d){
+
+//             return a + b + c + d + e;
+//         };
+//     };
+//     }
+    
+// }
+
+// const sum2 = sum(1);
+// const sum3 = sum2(2);
+// const sum4 = sum3(3);
+// const result = sum4(4);
+// console.log(result); 
+
+
+// function outer() {
+//     let getY;
+
+//     {
+//         const y = 6;
+//         getY = () => y;
+//     }
+//     console.log(typeof y);
+//     console.log(getY());
+
+// }
+
+// outer();
+
+let x = 5;
+export const getX = () => x;
+export const setX = (val) => {
+    x = val;
+};
 
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Closures#closure
